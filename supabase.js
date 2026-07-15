4aa6d730f0d8c88d1e534db9bbc3e65c8455ee8a
miticka.eu <nika6115@gmail.com>
2026-07-15 14:51:26 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+    "https://krzouuhouzzlvsygmalb.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_G5IG29m-XPhRhc3eB0Dy3Q__5Vcdflw";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
