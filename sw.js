d39049dc5b308556572c295bac970c4b9ff9da9a
miticka.eu <nika6115@gmail.com>
2026-08-05 09:21:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,103 @@
+const CACHE_NAME =
+    "obedy-tmv-v1";
 
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./supabase.js",
+    "./manifest.json",
+    "./assets/favicon.png"
+];
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+            caches
+                .open(CACHE_NAME)
+                .then(cache =>
+                    cache.addAll(
+                        FILES_TO_CACHE
+                    )
+                )
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+            caches
+                .keys()
+                .then(cacheNames =>
+                    Promise.all(
+                        cacheNames
+                            .filter(
+                                cacheName =>
+                                    cacheName !==
+                                    CACHE_NAME
+                            )
+                            .map(
+                                cacheName =>
+                                    caches.delete(
+                                        cacheName
+                                    )
+                            )
+                    )
+                )
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        if (
+            event.request.method !==
+            "GET"
+        ) {
+            return;
+        }
+
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+
+                    const responseClone =
+                        response.clone();
+
+                    caches
+                        .open(CACHE_NAME)
+                        .then(cache => {
+
+                            cache.put(
+                                event.request,
+                                responseClone
+                            );
+
+                        });
+
+                    return response;
+
+                })
+                .catch(() =>
+                    caches.match(
+                        event.request
+                    )
+                )
+        );
+
+    }
+);
