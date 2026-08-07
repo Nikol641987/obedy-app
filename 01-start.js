6528b8771c6ba13e7ac4e70f4110da0c8b39c677
miticka.eu <nika6115@gmail.com>
2026-08-07 10:53:46 +0200

    Update 01-start.js

--- 01-start.js
+++ 01-start.js
@@ -1,1 +1,25 @@
+// =====================================
+// OBEDY TMV
+// =====================================
 
+// =====================================
+// 1. Štart aplikácie
+// =====================================
+
+let selectedOrderDate = null;
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    await loadEmployees();
+    
+    updatePermissions();
+
+    setupNavigation();
+    setupLogin();
+    setupOrderButton();
+    setupManualIssue();
+    setupChipLogin();
+    setupChipIssue();
+    setupMonthlyReport();
+
+});
