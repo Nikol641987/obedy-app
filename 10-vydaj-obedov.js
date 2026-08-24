e0f3580bc8b407ccc2038a650fcb3654e4120af6
miticka.eu <nika6115@gmail.com>
2026-08-24 09:47:25 +0200

    Update 10-vydaj-obedov.js

--- 10-vydaj-obedov.js
+++ 10-vydaj-obedov.js
@@ -817,7 +817,7 @@ const menuText =
 const label =
     menuNumber === 6
         ? "⭐ Menu 6"
-        `Menu ${menuNumber}`
+        :`Menu ${menuNumber}`
 
 return `
     <div class="today-menu-row">
